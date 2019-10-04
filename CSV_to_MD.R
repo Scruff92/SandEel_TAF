8d4856aac7485deb3d70c3bbaa52a0c9018941c2
Scruff92 <ruairioggy@hotmail.com>
2019-10-04 10:46:18 +0200

    R script for csv to md

---
+++ CSV_to_MD.R
@@ -0,0 +1,16 @@
+#Script to convert .csv to MD table
+
+
+Path_to_CSV<-"~/Documents/ICES/Sandeel Reports/Tables_and_Figures.csv"
+
+
+md_table <- function(df){
+  paste0('|', paste(names(df), collapse = '|'), '|\n|', 
+         paste(rep('---', length(df)), collapse = '|'), '|\n|', 
+         paste(Reduce(function(x, y){paste(x, y, sep = '|')}, df), collapse = '|\n|'), '|')
+}
+
+TABLE<-read.csv(Path_to_CSV,header=T)
+
+
+writeLines(md_table(TABLE),paste0(substr(Path_to_CSV,start = 1,stop = nchar(Path_to_CSV)-3),"md"))
